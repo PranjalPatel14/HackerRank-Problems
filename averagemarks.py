9ef8e1c5b5ae6a1d000a1ea9f61910fc5a523d87
Pranjal Patel <patelpranjal139@gmail.com>
2023-09-05 10:31:32 +0530

    Added Nested List

---
+++ averagemarks.py
@@ -0,0 +1,6 @@
+dict1 = {
+    "Harsh": [25,26.5,28],
+    "Anurag": [26,28,30],
+}
+list1 = (dict1.get("Harsh"))
+print(sum(list1))
---
+++ listcomprehension.py
@@ -0,0 +1,9 @@
+# ls = [i for i in range(100) if i%3 == 0 ]
+# print(ls)
+
+# dict1 = {i:f"item{i}" for i in range(100) if i % 3 == 0}
+# print(dict1)
+
+dict1 = {i:f"item{i}" for i in range(100) if i % 3 == 0}
+print(dict1)
+
